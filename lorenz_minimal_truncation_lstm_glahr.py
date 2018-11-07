867f2d54f7f2eebf3d3682c986a34fdc14797dcc
Gustavo Lahr <gjgl.lahr@gmail.com>
2018-11-07 16:26:05 +0100

    algorithm working with multiple rnn layers and with input for my own data

--- lorenz_minimal_truncation_lstm_glahr.py
+++ lorenz_minimal_truncation_lstm_glahr.py
@@ -21,23 +21,42 @@ epochs = 200  # Maximum number of epochs
 lr = 0.01  # Learning rate
 
 # Load and arrange data
-raw_data = np.genfromtxt('data/lorenz1000.dt')
 
-
-# raw_data = np.genfromtxt('data/data_adjust-1.csv',delimiter = ",")
-# train_X = raw_data[1:T+1,0]
+raw_data = np.genfromtxt('data/data_adjust-1.csv',delimiter = ",")
+train_X = raw_data[0:T,9]
+train_X = train_X.copy()
+train_Y = raw_data[gap:T+gap,9]
+train_Y = train_Y.copy()
+test_X = raw_data[T:-gap,9]
+test_X = test_X.copy()
+test_Y = raw_data[T+gap:,9]
+test_Y = test_Y.copy()
 # print(train_X.size)
-# train_Y = raw_data[1+gap:T+gap+1,9]
+# print(type(train_X))
+# print(train_Y.size)
+# print(type(train_Y))
 
-train_X = raw_data[0:T]
-train_Y = raw_data[gap:T+gap]
+# raw_data = np.genfromtxt('data/lorenz1000.dt')
+# train_X = raw_data[0:T]
+# train_Y = raw_data[gap:T+gap]
+# test_X = raw_data[T:-gap]
+# test_Y = raw_data[T+gap:]
+# print(type(train_X))
+# print(train_X)
+# print(type(train_Y))
+# print(train_X.size)
+# print(train_Y.size)
 
-test_X = raw_data[T:-gap]
-test_Y = raw_data[T+gap:]
 train_X.resize(n, train_X.size, d)
 train_Y.resize(n, train_Y.size, m)
 test_X.resize(n_test, test_X.size, d)
 test_Y.resize(n_test, test_Y.size, m)
+# print("\n")
+# print(type(train_X))
+# print(type(train_Y))
+# print(train_X.size)
+# print(train_Y.size)
+# print(train_X)
 
 # Placeholders
 inputs = tf.placeholder(tf.float32, [None, None, d])
@@ -69,22 +88,22 @@ cell = tf.nn.rnn_cell.MultiRNNCell(cells)
 # print(([cell]*num_layers).state_size)
 # cell = tf.nn.rnn_cell.MultiRNNCell([cell for n in range(num_layers)], state_is_tuple=True)
 
-print("cell.state_size = " + str(cell.state_size))
-print("cell.get_config = " + str(cell.get_config))
+# print("cell.state_size = " + str(cell.state_size))
+# print("cell.get_config = " + str(cell.get_config))
 
 # A state with all variables set to zero
 zero_state = cell.zero_state(n, tf.float32)
 # print("zero_state = " + str(zero_state[0]))
 # State
 state =  nest.map_structure(lambda tensor: tf.Variable(tensor, trainable=False), zero_state)
-print("state0 ########################## = " + str(state[0]) + "state1 ########################## = " + str(state[1]))
+# print("state0 ########################## = " + str(state[0]) + "state1 ########################## = " + str(state[1]))
 
 # RNN
 rnn_output, new_state = tf.nn.dynamic_rnn(cell, inputs, initial_state=state, dtype=tf.float32)
-print("\nrnn_output.shape = " + str(rnn_output.shape))
-print("new_state.shape = " + str(new_state[1]))
-
-print("\n\n")
+# print("\nrnn_output.shape = " + str(rnn_output.shape))
+# print("new_state.shape = " + str(new_state[1]))
+#
+# print("\n\n")
 
 # State update
 update_state = nest.map_structure(tf.assign, state, new_state)
@@ -95,9 +114,9 @@ reset_state  = nest.flatten(reset_state)
 with tf.control_dependencies(update_state):  # Update_state is already a list
     rnn_output = tf.identity(rnn_output)
 
-print("rnn_output.shape = " + str(rnn_output.shape))
-
-print("\n\n")
+# print("rnn_output.shape = " + str(rnn_output.shape))
+#
+# print("\n\n")
 
 # Note the following reshaping:
 #   We want a prediction for every time step.
@@ -108,23 +127,23 @@ print("\n\n")
 
 # rnn_output_flat = tf.reshape(rnn_output, [-1, N])
 
-rnn_output_flat = tf.reshape(rnn_output, [-1, n_cells_layers[1]])
-print("\n\n")
-print("rnn_output_flat.get_shape = " + str(rnn_output_flat.get_shape()))
-print("\n\n")
+rnn_output_flat = tf.reshape(rnn_output, [-1, n_cells_layers[-1]])
+# print("\n\n")
+# print("rnn_output_flat.get_shape = " + str(rnn_output_flat.get_shape()))
+# print("\n\n")
 prediction_flat = tf.layers.dense(rnn_output_flat, m, activation=None)
-print("\n\n")
-print("prediction_flat.get_shape = " + str(prediction_flat.get_shape()))
-print("\n\n")
+# print("\n\n")
+# print("prediction_flat.get_shape = " + str(prediction_flat.get_shape()))
+# print("\n\n")
 
 targets_flat = tf.reshape(targets, [-1, m])
-print("\n\n")
-print("targets_flat.get_shape = " + str(targets_flat.get_shape()))
-print("\n\n")
+# print("\n\n")
+# print("targets_flat.get_shape = " + str(targets_flat.get_shape()))
+# print("\n\n")
 prediction  = tf.reshape(prediction_flat, [-1, tf.shape(inputs)[1], m])
-print("\n\n")
-print("prediction.get_shape = " + str(prediction.get_shape()))
-print("\n\n")
+# print("\n\n")
+# print("prediction.get_shape = " + str(prediction.get_shape()))
+# print("\n\n")
 #my changes
 
 # print(rnn_output_flat.get_shape())
@@ -136,6 +155,8 @@ print("\n\n")
 print("loss.get_shape = " + str(loss.get_shape()))
 print("\n\n")
 train_step = tf.train.AdamOptimizer(lr).minimize(loss)
+
+time = np.arange(train_X.size)*0.012
 
 # Create session and initialize variables
 with tf.Session() as sess:
@@ -164,8 +185,8 @@ with tf.Session() as sess:
     model, final_state = sess.run([prediction, new_state], feed_dict={inputs: train_X})
     plt.rc('font', size=14)
     # plt.plot(train_X[0,:,0], label='input', color='lightgray', linestyle='--')
-    plt.plot(train_Y[0,:,0], label='target', linestyle='-', linewidth=3)
-    plt.plot(model[0,:,0], label='model', linestyle='-', linewidth=3)
+    plt.plot(time, train_Y[0,:,0], label='target', linestyle='-', linewidth=3)
+    plt.plot(time, model[0,:,0], label='model', linestyle='-', linewidth=3)
     plt.legend(loc=1)
     plt.xlabel('time [t]')
     plt.ylabel('signal')
@@ -182,8 +203,8 @@ with tf.Session() as sess:
         concatenated.append(model)
     model = np.stack(concatenated, axis=0)
     model = np.reshape(model, model.size)
-    plt.plot(train_Y[0,:,0], label='target', linestyle='-', linewidth=3)
-    plt.plot(model, label='model', linestyle='-', linewidth=3)
+    plt.plot(time, train_Y[0,:,0], label='target', linestyle='-', linewidth=3)
+    plt.plot(time, model, label='model', linestyle='-', linewidth=3)
     plt.legend(loc=1)
     plt.xlabel('time [t]')
     plt.ylabel('signal')
